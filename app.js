f95e6ff9db46ac0a464eec0e35a24fa71cfb47a2
joelmaharjan <joelmaharjan23@gmail.com>
2023-12-14 18:47:36 +0545

    added middleware and style

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const express = require("express");
 //express app
 const app = express();
 
+//morgan
+const morgan = require('morgan')
+
 //listening for request
 app.set("view engine", "ejs");
 app.listen(3000);
+
+app.use(morgan('dev'))
+
+//static & public files
+
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -22,14 +31,14 @@ app.get("/", (req, res) => {
       snippet: "hope is hope",
     },
   ];
-  res.render("index", {title:"Home", blogs});
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about",{title:"About"}, );
+  res.render("about", { title: "About" });
 });
-app.get("/create", (req, res) => {
-  res.render("create",{title:"Create A blog"});
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create A blog" });
 });
 
 //redirect
