1111150f4cf49ce84a714b9c5caddd80b38ab1f0
joelmaharjan <joelmaharjan23@gmail.com>
2023-12-16 20:58:04 +0545

    connected to mongoDb using mongoose and added data

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
 const express = require("express");
+//morgan
+const morgan = require("morgan");
 
+const mongoose = require("mongoose");
+
+const Blog = require("./module/blog");
 //express app
 const app = express();
 
-//morgan
-const morgan = require('morgan')
+//connect to mongodb
+
+const dbURI =
+  "mongodb+srv://joelmaharjan:Joel621303@cluster0.wsrmjmj.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("connected to mongodb"))
+  .catch((err) => err);
 
 //listening for request
 app.set("view engine", "ejs");
 app.listen(3000);
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 //static & public files
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Hope",
-      snippet: "hope is hope",
-    },
-    {
-      title: "Hope",
-      snippet: "hope is hope",
-    },
-    {
-      title: "Hope",
-      snippet: "hope is hope",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find().then((result) => {
+    res.render('index', {title:"All Blogs", blogs: result});
+  }).catch((err)=>{
+    console.log(err)
+  })
 });
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create A blog" });
